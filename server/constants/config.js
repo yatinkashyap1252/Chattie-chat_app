71ceed6f37cd5cadbfc628c62e0d8556fe63faa5
Yatin Kashyap <156793299+yatinkashyap1252@users.noreply.github.com>
2025-07-15 23:22:13 +0530

    Update config.js

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000",process.env.CLIENT_URL],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
